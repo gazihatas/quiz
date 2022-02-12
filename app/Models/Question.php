9b64656ab88d4df4274b9f22cebe972cd1f325ad
Gazi HATAŞ <gazihatas71@gmail.com>
2022-02-12 20:38:18 +0300

    Dosya Yükleme İşlemleri, File Upload, Soru Oluşturma.

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
-{
+{   
+    protected $fillable =['question', 'answer1','answer2','answer3','answer4','correct_answer','image'];
+
     use HasFactory;
 }
